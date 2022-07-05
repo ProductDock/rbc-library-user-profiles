ea32d09b82eed55cb8e319fd7a033c3776c7c55f
Dusan <dusan.petrovic@productdock.com>
2022-07-05 13:14:45 +0200

    Added initial spring boot project

---
+++ src/main/java/com/productdock/RbcLibraryUserProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.productdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbcLibraryUserProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbcLibraryUserProfilesApplication.class, args);
+	}
+
+}
