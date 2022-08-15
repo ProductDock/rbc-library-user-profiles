f38350c88e01c2aec2e30ac8d8a269b10b33114f
nenadbecanovicpd <nenad.becanovic@productdock.com>
2022-08-15 15:52:50 +0200

    Add prod-env start script

--- prod-env/update-user-profiles-image.sh
+++ prod-env/update-user-profiles-image.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 sudo usermod -a -G docker ${USER}
 docker-credential-gcr configure-docker
-
 docker stop rbc-library-user-profiles
 docker rm rbc-library-user-profiles
 docker rmi $(docker images | grep "rbc-library-user-profiles")
 
-export GOOGLE_APPLICATION_CREDENTIALS=/home/pd-library/credentials.json
-docker run -v $GOOGLE_APPLICATION_CREDENTIALS:/home/credentials.json:ro -e GOOGLE_APPLICATION_CREDENTIALS=/home/credentials.json -dp 8082:8080 --name=rbc-library-user-profiles gcr.io/prod-pd-library/rbc-library-user-profiles:$1
+docker run -v $USER_PROFILES_JWT_PUBLIC_KEY:/home/app.pub:ro -v $USER_PROFILES_JWT_PRIVATE_KEY:/home/app.key:ro -e USER_PROFILES_JWT_PUBLIC_KEY=/home/app.pub -e USER_PROFILES_JWT_PRIVATE_KEY=/home/app.key -dp 8085:8080 --name=rbc-library-user-profiles gcr.io/prod-pd-library/rbc-library-user-profiles:$1
